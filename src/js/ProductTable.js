482b6745b7f2c6e3a72e7e86ba9a66a1691400ec
KimPhucNguyen <phamthibong1503@gmail.com>
2022-05-14 13:51:05 +0700

    fetch data

--- src/js/ProductTable.js
+++ src/js/ProductTable.js
@@ -1,14 +1,28 @@
 const columnDefs = [
-    { field: 'Status' },
+    {
+        field: 'Status',
+        cellStyle: params => {
+            if (params.value === 'Invalid') {
+                return { color: 'red' };
+            }
+            if (params.value === 'Published') {
+                return { color: 'green' };
+            }
+            if (params.value === 'Pending') {
+                return { color: 'gray' };
+            }
+            return null;
+        }
+    },
     { field: 'Quote Number', minWidth: 180 },
-    { field: 'Agreement Name', minWidth: 200 },
+    { field: 'Agreement Name', minWidth: 200, cellStyle: {color: 'blue'} },
     { field: 'Agreement Type', minWidth: 200 },
     { field: 'Distributor Name', minWidth: 200 },
-    { field: 'Effective Date', minWidth: 200 },
-    { field: 'Effective Date', minWidth: 200 },
-    { field: 'Expiration Date', minWidth: 180 },
+    { field: 'Effective Date', minWidth: 200, type: ['dateColumn', 'nonEditableColumn'] },
+    { field: 'Effective Date', minWidth: 200, type: ['dateColumn', 'nonEditableColumn'] },
+    { field: 'Expiration Date', minWidth: 180, type: ['dateColumn', 'nonEditableColumn'] },
     { field: 'Create Date', minWidth: 200 },
-    { field: 'Days Until Expiration', minWidth: 200 }
+    { field: 'Days Until Expiration', minWidth: 200, type: 'numberColumn' }
 ];
 
 const gridOptions = {
@@ -23,6 +37,10 @@ const gridOptions = {
         enablePivot: false,
         sortable: true,
         filter: true,
+        editable: true,
+        filter: 'agTextColumnFilter',
+        floatingFilter: true,
+        resizable: true,
     },
     sideBar: {
         toolPanels: [
@@ -39,9 +57,37 @@ const gridOptions = {
             }
         ]
     },
+    columnTypes: {
+        numberColumn: { width: 130, filter: 'agNumberColumnFilter' },
+        medalColumn: { width: 100, columnGroupShow: 'open', filter: false },
+        nonEditableColumn: { editable: false },
+        dateColumn: {
+            filter: 'agDateColumnFilter',
+
+            filterParams: {
+                comparator: (filterLocalDateAtMidnight, cellValue) => {
+                    const dateParts = cellValue.split('/');
+                    const day = Number(dateParts[0]);
+                    const month = Number(dateParts[1]) - 1;
+                    const year = Number(dateParts[2]);
+                    const cellDate = new Date(year, month, day);
+
+                    if (cellDate < filterLocalDateAtMidnight) {
+                        return -1;
+                    } else if (cellDate > filterLocalDateAtMidnight) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                },
+            },
+        },
+    },
+    rowData: null,
 };
 
-// setup the grid after the page has finished loading
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
